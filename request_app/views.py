5234e6367d6b0bf71330c9dd48e2b493fb2316c2
thi-mateus <thiagomateus.ufrn@gmail.com>
2023-08-30 14:52:03 -0300

    list_requests e save_request UPD

--- request_app/views.py
+++ request_app/views.py
@@ -3,32 +3,34 @@ from django.views.generic import ListView, DetailView
 from django.views import View
 from django.http import HttpResponse
 from django.contrib import messages
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 from service_app.models import Service
 from profile_app.models import Military
 from .models import Request
-from django.utils import timezone
 
 
-class ListRequests(ListView):
+class RequestMixin:
+    @method_decorator(login_required(login_url='profile:create'))
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+
+class ListRequests(RequestMixin, ListView):
     template_name = 'request/list_requests.html'
     context_object_name = 'request_list'
 
-    def dispatch(self, *args, **kwargs):
-        if not self.request.user.is_authenticated:
-            messages.error(self.request, 'Você precisa fazer login.')
-            return redirect('profile:create')
-        return super().dispatch(*args, **kwargs)
-
     def get_queryset(self):
-        military_instance = Military.objects.filter(
-            usuario=self.request.user).first()
+        military_instance = Military.objects.get(
+            usuario=self.request.user)
         return Request.objects.filter(id_mil=military_instance, status='S')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        military_instance = Military.objects.filter(
-            usuario=self.request.user).first()
+        military_instance = Military.objects.get(
+            usuario=self.request.user)
         context['military'] = military_instance
         return context
 
@@ -36,23 +38,9 @@ class ListRequests(ListView):
 class SaveRequest(View):
     template_name = 'request/saverequest.html'
 
+    @method_decorator(login_required(login_url='profile:create'))
     def dispatch(self, *args, **kwargs):
-        if not self.request.user.is_authenticated:
-            messages.error(self.request, 'Você precisa fazer login.')
-            return redirect('profile:create')
-
-        self.military_instance = Military.objects.filter(
-            usuario=self.request.user).first()
-
         return super().dispatch(*args, **kwargs)
-
-    def check_authentication(self):
-        if not self.request.user.is_authenticated:
-            messages.error(
-                self.request,
-                'Você precisa fazer login.'
-            )
-            return redirect('profile:create')
 
     def check_cart(self):
         if not self.request.session.get('cart'):
@@ -62,8 +50,13 @@ class SaveRequest(View):
             )
             return redirect('service:list')
 
-    def check_service_status(self, service):
+    def check_service_status_and_cart(self, service):
         if service.status != 'A':
+            service_id = str(service.id)
+            if service_id in self.request.session['cart']:
+                del self.request.session['cart'][service_id]
+                self.request.session.save()
+
             messages.error(
                 self.request,
                 f'O serviço "{service.local} - {service.data_inicio}" '
@@ -71,9 +64,33 @@ class SaveRequest(View):
             )
             return True
 
+        service_id = str(service.id)
+        if self.check_service_already_selected(service_id):
+            del self.request.session['cart'][service_id]
+            self.request.session.save()
+            messages.error(
+                self.request,
+                f'O serviço "{service.local} - {service.data_inicio}" '
+                f'já foi adicionado à sua solicitação.'
+            )
+            return True
+
+        return False
+
+    def check_service_already_selected(self, service_id):
+        military_instance = Military.objects.get(
+            usuario=self.request.user)
+        user_requests = Request.objects.filter(
+            id_mil=military_instance,
+            id_sv=service_id
+        )
+
+        if user_requests.exists():
+            return user_requests
+
     def create_requests(self, cart):
-        military_instance = Military.objects.filter(
-            usuario=self.request.user).first()
+        military_instance = Military.objects.get(
+            usuario=self.request.user)
         current_timezone = timezone.get_current_timezone()
 
         requests_to_create = [
@@ -90,40 +107,24 @@ class SaveRequest(View):
 
         Request.objects.bulk_create(requests_to_create)
 
-    def check_service_already_selected(self, cart, service_id):
-        for cart_item in cart.values():
-            if cart_item['service_id'] == service_id:
-                return True
-
-        user_requests = Request.objects.filter(
-            id_mil=self.military_instance,
-            id_sv=service_id
-        ).exists()
-        return user_requests
+        messages.success(
+            self.request,
+            'A solicitação foi salva com sucesso!.'
+        )
 
     def get(self, *args, **kwargs):
-        self.check_authentication()
         self.check_cart()
 
         cart = self.request.session.get('cart')
-        cart_service_ids = [sv for sv in cart]
+        cart_service_ids = [sv['service_id'] for sv in cart.values()]
         db_services = list(
             Service.objects.filter(id__in=cart_service_ids)
         )
 
         for service in db_services:
-            if self.check_service_status(service):
-                del self.request.session['cart'][str(service.id)]
+            if self.check_service_status_and_cart(service):
                 self.request.session.save()
                 return redirect('service:cart')
-
-            if self.check_service_already_selected(cart, service.id):
-                messages.error(
-                    self.request,
-                    f'O serviço "{service.local} - {service.data_inicio}" '
-                    f'já foi adicionado à sua solicitação.'
-                )
-                return redirect('request:list_requests')
 
         self.create_requests(cart)
         del self.request.session['cart']
