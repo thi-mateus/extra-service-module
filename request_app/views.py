e7672b66c27236328687ddd5c3bf50ce0e15eb5d
thi-mateus <thiagomateus.ufrn@gmail.com>
2023-08-30 13:05:15 -0300

    save_request ok

--- request_app/views.py
+++ request_app/views.py
@@ -7,8 +7,7 @@ from django.contrib import messages
 from service_app.models import Service
 from profile_app.models import Military
 from .models import Request
-from datetime import datetime
-import pytz
+from django.utils import timezone
 
 
 class ListRequests(ListView):
@@ -37,7 +36,17 @@ class ListRequests(ListView):
 class SaveRequest(View):
     template_name = 'request/saverequest.html'
 
-    def get(self, *args, **kwargs):
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(self.request, 'Você precisa fazer login.')
+            return redirect('profile:create')
+
+        self.military_instance = Military.objects.filter(
+            usuario=self.request.user).first()
+
+        return super().dispatch(*args, **kwargs)
+
+    def check_authentication(self):
         if not self.request.user.is_authenticated:
             messages.error(
                 self.request,
@@ -45,6 +54,7 @@ class SaveRequest(View):
             )
             return redirect('profile:create')
 
+    def check_cart(self):
         if not self.request.session.get('cart'):
             messages.error(
                 self.request,
@@ -52,52 +62,72 @@ class SaveRequest(View):
             )
             return redirect('service:list')
 
+    def check_service_status(self, service):
+        if service.status != 'A':
+            messages.error(
+                self.request,
+                f'O serviço "{service.local} - {service.data_inicio}" '
+                f'não está mais aberto para solicitações.'
+            )
+            return True
+
+    def create_requests(self, cart):
+        military_instance = Military.objects.filter(
+            usuario=self.request.user).first()
+        current_timezone = timezone.get_current_timezone()
+
+        requests_to_create = [
+            Request(
+                id_mil=military_instance,
+                id_sv=get_object_or_404(Service, id=v['service_id']),
+                id_opcao=1,
+                data_solicitacao=timezone.localtime(
+                    timezone.now(), current_timezone),
+                status='S',
+                criterio='',
+            ) for v in cart.values()
+        ]
+
+        Request.objects.bulk_create(requests_to_create)
+
+    def check_service_already_selected(self, cart, service_id):
+        for cart_item in cart.values():
+            if cart_item['service_id'] == service_id:
+                return True
+
+        user_requests = Request.objects.filter(
+            id_mil=self.military_instance,
+            id_sv=service_id
+        ).exists()
+        return user_requests
+
+    def get(self, *args, **kwargs):
+        self.check_authentication()
+        self.check_cart()
+
         cart = self.request.session.get('cart')
         cart_service_ids = [sv for sv in cart]
-        db_service = list(
+        db_services = list(
             Service.objects.filter(id__in=cart_service_ids)
         )
 
-        for service in db_service:
-            sid = str(service.id)
-            status = cart[sid]['service_status']
-
-            # Verificar se o serviço que está no carrinho ainda está aberto(A)
-            if service.status != 'A':
-
-                messages.error(
-                    self.request,
-                    f'O serviço "{cart[sid]["service_local"]} - {cart[sid]["service_data_inicio"]}" '
-                    f'não está mais aberto para solicitações.'
-                )
-                # Serviço não está aberto(A): deletar do carrinho!
-                del self.request.session['cart'][sid]
+        for service in db_services:
+            if self.check_service_status(service):
+                del self.request.session['cart'][str(service.id)]
                 self.request.session.save()
-
                 return redirect('service:cart')
 
-        request = Request()
-        # print(request.pk)
+            if self.check_service_already_selected(cart, service.id):
+                messages.error(
+                    self.request,
+                    f'O serviço "{service.local} - {service.data_inicio}" '
+                    f'já foi adicionado à sua solicitação.'
+                )
+                return redirect('request:list_requests')
 
-        Request.objects.bulk_create(
-            [
-                Request(
-                    id_mil=Military.objects.filter(
-                        usuario=self.request.user).first(),
-                    id_sv=get_object_or_404(
-                        Service, id=v['service_id']),
-                    id_opcao=1,
-                    data_solicitacao=pytz.timezone(
-                        'America/Sao_Paulo').localize(datetime.now()),
-                    status='S',
-                    criterio='',
-                ) for v in cart.values()
-            ]
-        )
-
+        self.create_requests(cart)
         del self.request.session['cart']
-
-        return redirect('request:list_request')
+        return redirect('request:list_requests')
 
 
 class List(View):
